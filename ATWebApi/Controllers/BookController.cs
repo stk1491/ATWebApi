b1a8782848dc9627b8f59021b2f86fb177c93253
ue-01 <ue-01@UE015>
2023-08-21 17:46:31 +0400

    Add project files.

---
+++ ATWebApi/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace ATWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private static List<string> listbook = new List<string>() {"Harry Potter","Notebook" };
+        [HttpGet]
+        public 
+    }
+}
